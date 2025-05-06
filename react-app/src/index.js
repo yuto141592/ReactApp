4946944c3dac157cc19e812208b8dec9b226e29c
yuto141592 <aricchi0208@icloud.com>
2025-05-07 03:23:00 +0900

    reset

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css'; // Tailwind を含む
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
